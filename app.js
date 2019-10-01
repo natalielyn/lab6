5ac7472a275ee302e560a6597a86c8e422a9d800
nataliealway <nlmaxwell@gmail.com>
2019-09-30 18:36:40 -0700

    Added structure to both index and sales html. added my city objects in javascript

--- app.js
+++ app.js
@@ -1,2 +1,139 @@
 'use strict';
 
+var seattle = {
+    location: 'seattle',
+    minCustHr: 23,
+    maxCustHr: 65,
+    avgCookiesPerCust: 6.3,
+    cookiesPurchasePerHour:[],
+
+    avgCustomersPerHour: function () {
+        return Math.floor(Math.random() * (this.maxCustHr - this.minCustHr + 1)) + this.minCustHr; 
+      },
+      avgCookieTotal: function () {
+        for (var i=0; i < 15; i++) {
+            this.cookiesPurchasePerHour.push(math.floor(this.avgCustomersPerHour() * this.avgCookiesPerCust)); 
+        }
+      },
+      getTotalCookies: function(){
+        var totalCookies = 0;
+        for (var i = 0; i < this.cookiesPurchasePerHour.length; i++) {
+          totalCookies = totalCookies + this.cookiesPurchasePerHour[i];
+        }
+        return totalCookies;
+      },
+    }; 
+
+
+
+
+    var seattle = {
+      location: 'tokyo',
+      minCustHr: 3,
+      maxCustHr: 24,
+      avgCookiesPerCust: 1.2,
+      cookiesPurchasePerHour:[],
+  
+      avgCustomersPerHour: function () {
+          return Math.floor(Math.random() * (this.maxCustHr - this.minCustHr + 1)) + this.minCustHr; 
+        },
+        avgCookieTotal: function () {
+          for (var i=0; i < 15; i++) {
+              this.cookiesPurchasePerHour.push(math.floor(this.avgCustomersPerHour() * this.avgCookiesPerCust)); 
+          }
+        },
+        getTotalCookies: function(){
+          var totalCookies = 0;
+          for (var i = 0; i < this.cookiesPurchasePerHour.length; i++) {
+            totalCookies = totalCookies + this.cookiesPurchasePerHour[i];
+          }
+          return totalCookies;
+        },
+      }; 
+
+
+      var dubai = {
+        location: 'dubai',
+        minCustHr: 11,
+        maxCustHr: 38,
+        avgCookiesPerCust: 3.7,
+        cookiesPurchasePerHour:[],
+    
+        avgCustomersPerHour: function () {
+            return Math.floor(Math.random() * (this.maxCustHr - this.minCustHr + 1)) + this.minCustHr; 
+          },
+          avgCookieTotal: function () {
+            for (var i=0; i < 15; i++) {
+                this.cookiesPurchasePerHour.push(math.floor(this.avgCustomersPerHour() * this.avgCookiesPerCust)); 
+            }
+          },
+          getTotalCookies: function(){
+            var totalCookies = 0;
+            for (var i = 0; i < this.cookiesPurchasePerHour.length; i++) {
+              totalCookies = totalCookies + this.cookiesPurchasePerHour[i];
+            }
+            return totalCookies;
+          },
+        }; 
+
+
+        var paris = {
+          location: 'paris',
+          minCustHr: 23,
+          maxCustHr: 38,
+          avgCookiesPerCust: 2.3,
+          cookiesPurchasePerHour:[],
+      
+          avgCustomersPerHour: function () {
+              return Math.floor(Math.random() * (this.maxCustHr - this.minCustHr + 1)) + this.minCustHr; 
+            },
+            avgCookieTotal: function () {
+              for (var i=0; i < 15; i++) {
+                  this.cookiesPurchasePerHour.push(math.floor(this.avgCustomersPerHour() * this.avgCookiesPerCust)); 
+              }
+            },
+            getTotalCookies: function(){
+              var totalCookies = 0;
+              for (var i = 0; i < this.cookiesPurchasePerHour.length; i++) {
+                totalCookies = totalCookies + this.cookiesPurchasePerHour[i];
+              }
+              return totalCookies;
+            },
+          }; 
+
+
+          var lima = {
+            location: 'lima',
+            minCustHr: 2,
+            maxCustHr: 38,
+            avgCookiesPerCust: 4.6,
+            cookiesPurchasePerHour:[],
+        
+            avgCustomersPerHour: function () {
+                return Math.floor(Math.random() * (this.maxCustHr - this.minCustHr + 1)) + this.minCustHr; 
+              },
+              avgCookieTotal: function () {
+                for (var i=0; i < 15; i++) {
+                    this.cookiesPurchasePerHour.push(math.floor(this.avgCustomersPerHour() * this.avgCookiesPerCust)); 
+                }
+              },
+              getTotalCookies: function(){
+                var totalCookies = 0;
+                for (var i = 0; i < this.cookiesPurchasePerHour.length; i++) {
+                  totalCookies = totalCookies + this.cookiesPurchasePerHour[i];
+                }
+                return totalCookies;
+              },
+            }; 
+
+var locationsListElement = document.getElementById('locationslist')
+
+//seattle
+
+
+      
+
+    
+    
+    
+
