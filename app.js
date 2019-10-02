2d606d7bab90124aa2635cdc1eeaff933bcb3c7d
nataliealway <nlmaxwell@gmail.com>
2019-10-02 14:17:52 -0700

    fixed typos in table header code, was able to get table header to show on webpage, yay!

--- app.js
+++ app.js
@@ -1,51 +1,14 @@
 'use strict';
  
-
-var locationHours = ['6am','7am','8am','9am','10am','11am','12am','1pm','2pm','3pm','4pm','5pm','6pm','7pm',
-];
+var locationHours = ['6am','7am','8am','9am','10am','11am','12am','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
 var initialNumStores = 0;
-var InitialTotalTotal = 0;
-
-// Number generator functions
-Location.prototype.randomCustomers = function() {
-  return Math.floor(Math.random() * (this.maxCustHr - this.minCustHr + 1) + this.minCustHr);
-};
-
-
-Location.prototype.randomCookieSales = function() {
-   for (var i = 0; i < hoursOpen; i++) {
-     this.cookiesPurchasePerHour.push(Math.floor(this.randomCustomers() * this.avgCookiesPerCustomer)); this.initialTotalSales += this.hourlySalesArray
-  }
-    return result;
- };
-
-Location.prototype.totalCookieSales = function() {
-  var cookieSalesSum = 0;
-  for (var i = 0; i < 15; i++) {
-  cookieSalesSum += this.randomCookieSales()[i];
-}
-  return cookieSalesSum; 
-};
-
-
-    function render(){
-    var storelistElement = document.getElementById('locationdata');
-    }
-    var storelistElement = document.getElementById('locationdata');
-
-  
-    var article = document.createElement('article');
-    article.setAttribute('id', `${this.locationdata}`);
-    storelistElement.appendChild(article);
-
-    var h2 = document.createElement('h2');
-    h2.textContent = this.storelist;
-    article.appendChild(h2);
+console.log(locationHours)
 
 //Table for data
   
 function renderTableHead(){
-  var mainElement =document.getElementById('locationdata');
+  var mainElement = document.getElementById('locationdata');
+  console.log(locationdata);
   var tableElement = document.createElement('table')
   tableElement.setAttribute('id', 'table');
   mainElement.appendChild(tableElement);
@@ -65,9 +28,9 @@ function renderTableHead(){
   tableHeadingElement.textContent = 'Location';
   tableRowElement.appendChild(tableHeadingElement);
 
-  for(var i = 0; i < hoursOpenTitle.length; i++){
+  for(var i = 0; i < locationHours.length; i++){
     tableHeadingElement = document.createElement('th');
-    tableHeadingElement.textContent = `${hours[i]}`;
+    tableHeadingElement.textContent = `${locationHours[i]}`;
     tableRowElement.appendChild(tableHeadingElement);
 }
   tableHeadingElement = document.createElement('th');
@@ -89,11 +52,13 @@ function renderTableFooter(){
   tableHeadingElement.textContent = 'Hourly Total: ';
   tableHeadingElement.setAttribute('scope', 'row');
   tableRowElement.appendChild(tableHeadingElement);
-}
+};
 
-tableDataElement.textContent = `${initialTotalTotal}`;
-tableRowElement.appendChild(tableDataElement);
 
+renderTableHead(); 
+
+
+//Location object
 function Location(locationcity, minCustHr, maxCustHr, avgCookiesPerCust, locationHours, hourlySalesArray, initialTotalSales) {
   this.locationcity = locationcity
   this.minCustHr = minCustHr
@@ -104,23 +69,57 @@ function Location(locationcity, minCustHr, maxCustHr, avgCookiesPerCust, locatio
   this.initialTotalSales = initialTotalSales;
 };
 
+// Number generator functions
+// Location.prototype.randomCustomers = function() {
+//   return Math.floor(Math.random() * (this.maxCustHr - this.minCustHr + 1) + this.minCustHr);
+// };
+
+
+// Location.prototype.randomCookieSales = function() {
+//    for (var i = 0; i < 15; i++) {
+//      this.hourlySalesArray.push(Math.floor(this.randomCustomers() * this.avgCookiesPerCustomer)); this.initialTotalSales += this.hourlySalesArray[i];
+//   }
+//     return result;
+//  };
+
+// Location.prototype.totalCookieSales = function() {
+//   var cookieSalesSum = 0;
+//   for (var i = 0; i < 15; i++) {
+//   cookieSalesSum += this.randomCookieSales()[i];
+// }
+//   return cookieSalesSum; 
+// };
+
+// Location.prototype.renderSalesDataAsTableRow = function(){
+//   var targetLocationNode = document.getElementById('table');
+//   var tableRowElement = document.createElement('tr');
+//   targetLocationNode.appendChild(tableRowElement);
+
+//   var tableHeadingElement = document.createElement('th');
+//   tableHeadingElement.setAttribute('scope', 'row');
+//   tableHeadingElement.textContent = `${this.locationcity}:`;
+//   tableRowElement.appendChild(tableHeadingElement);
+// }
+//   for(var i = 0; i < this.hourlySalesArray.length; i++){
+//     var tableDataElement = document.createElement('td');
+//     tableDataElement.setAttribute('class', `${locationHours[i]}`);
+//     tableDataElement.textContent = `${this.hourlySalesArray[i]}`;
+//     tableRowElement.appendChild(tableDataElement);
+      
+ 
+  
+  
+
    
-    var seattle = new Location('Seattle', 23, 65, 6.3, hours);
-    var tokyo = new Location('Toyko', 3, 24, 1.2, hours);
-    var dubai = new Location('Dubai', 11, 38, 3.7, hours);
-    var paris = new Location('Paris', 20, 38, 2.3, hours);
-    var lima = new Location('Lima', 2, 16, 4.6, hours);
+//     var seattle = new Location('Seattle', 23, 65, 6.3, hours);
+//     var tokyo = new Location('Toyko', 3, 24, 1.2, hours);
+//     var dubai = new Location('Dubai', 11, 38, 3.7, hours);
+//     var paris = new Location('Paris', 20, 38, 2.3, hours);
+//     var lima = new Location('Lima', 2, 16, 4.6, hours);
 
 
 
 //Executing Code
 
-renderTableFooter();
-
-
-    
-
-
-
-
-
+// renderTableFooter();
+  
