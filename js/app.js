dc1b39d3ec47108ec2682730111969e62bdf20a6
nataliealway <nlmaxwell@gmail.com>
2019-12-01 10:44:20 -0800

    converted functions to jquery, need to get data to render to body

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 'use strict';
 
-var locationHours = ['6am','7am','8am','9am','10am','11am','12am','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
-var locationSalesByHour = [];
-var locationData = [
+$(document).ready(() => {
+
+const locationHours = ['6am','7am','8am','9am','10am','11am','12am','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
+const locationSalesByHour = [];
+const locationData = [
   [`Seattle`, 23, 65, 6.3, [], 0],
   [`Tokyo`, 3, 24, 1.2, [], 0],
   [`Dubai`, 11, 38, 2.3, [], 0],
@@ -10,9 +12,9 @@ var locationData = [
   [`Lima`, 2, 16, 4.6, [], 0],
 ];
 
-var locationArray = [];
+const locationArray = [];
 
-var Location = function (locationcity, minCustHr, maxCustHr, avgCookiesPerCust, salesByHour, totalDailySales) {
+const Location = function (locationcity, minCustHr, maxCustHr, avgCookiesPerCust, salesByHour, totalDailySales) {
   this.locationcity = locationcity;
   this.minCustHr = minCustHr;
   this.maxCustHr = maxCustHr;
@@ -41,119 +43,79 @@ Location.prototype.avgCustomersPerHour = function() {
     };
 };
 
+
 //TABLEHEADER
-function renderTableHead() {
-  var section = document.getElementById ('locationSalesLists');
-  var table = document.createElement('table');
-  table.setAttribute ('id', 'table');
-  section.appendChild(table);
+const renderTableHead = function() {
+  $('<section>').appendTo('#locationSalesLists');
+  $('<table>').attr('id', 'salesTable').appendTo('#locationSalesLists section');
+  $('<thead>').appendTo('#salesTable');
+  $('<tr>').appendTo('thead');
+  $('<th>').appendTo('tr');
 
-
-  var tableHead = document.createElement('th');
-  tableHead.textContent = 'Location';
-  table.appendChild(tableHead);
-
-  var tableRow = document.createElement('tr');
-  tableHead.appendChild(tableRow);
-  
-  var th = document.createElement('th');
-  th.textContent = ``;
-  tableRow.appendChild(th);
-
-  for(var i = 0; i < locationHours.length; i++ ) {
-    var th = document.createElement('th');
-    th.textContent = `${locationHours[i]}`;
-    tableRow.appendChild(th);
-  }
-  var thLocationTotal = document.createElement('th');
-  thLocationTotal.textContent = `Daily Location Total`;
-  tableRow.appendChild(thLocationTotal);
-
+  locationHours.forEach(hr => {
+    $('tr').append(`<th>${hr}</th>`);
+  });
+  $('tr').append('<th>Daily Total</th>');
 };
 
-  function renderTableFoot() {
-    var table = document.getElementById('locationSalesLists');
+//TABLE FOOTER
+const renderTableFoot = function() {
+  $('<tfoot>').appendTo('#salesTable');
+  $('<tr>').appendTo('tfoot');
+  $('<td>').appendTo('tfoot tr').text('Totals');
 
-    var tfoot = document.createElement('tfoot');
-    table.appendChild(tfoot);
-  
-    var tr = document.createElement('tr');
-    tr.setAttribute('id','totals');
-    tfoot.appendChild(tr);
-  
-    var td = document.createElement('td');
-    td.textContent = 'Hourly Total';
-    tr.appendChild(td);
+  let finalTotal = 0;
+    for (let i = 0; i < locationHours.length; i++) {
+      let hourTotal = 0;
+      for (let j = 0; j < locationSalesByHour.length; j++) {
+        hourTotal += parseInt(locationSalesByHour[j][i]); 
+   }
+   finalTotal += hourTotal;
+   $('tfoot tr').append(`<td>${hourTotal}</td>`);
+    }
+    $('tfoot tr').append(`<td>${finalTotal}</td>`);
+  }
 
-    var finalTotal = 0;
-    for (var i = 0; i < locationHours.length; i++) {
-      var hourTotal = 0;
-      for (var j = 0; j < locationSalesByHour.length; j++) {
-        hourTotal += parseInt(locationSalesByHour[j][i]);
-      }
-      finalTotal += hourTotal;
-      td = document.createElement('td');
-      td.textContent = hourTotal;
-      tr.appendChild(td);
-      }
-      td = document.createElement('td');
-      td.textContent = finalTotal;
-      tr.appendChild(td);
-  };
+
 
 Location.prototype.renderSalesData = function() {
-    var table = document.getElementById('locationSalesLists');
-    var tr = document.createElement('tr');
-    table.appendChild(tr);
-    var th = document.createElement('th');
-    th.textContent = `${this.locationcity}:`;
-    tr.appendChild(th);
-
-    for(var i = 0; i < locationHours.length; i++){
-      var sales = this.salesByHour[i];
-      var td = document.createElement('td');
-      td.textContent = sales;
-      tr.appendChild(td);
-}
-
-  td = document.createElement('td');
-  td.setAttribute('class', 'total');
-  td.textContent = `${this.totalDailySales}`;
-  tr.appendChild(td);
-
+  $('tbody').append('<tr>');
+  $('tbody tr').append(`<th>${this.location}</th>`);
+  for(let i = 0; i < locationHours.length; i++){
+    let sales = this.locationSalesByHour[i];
+    $('tbody tr').append(`<td>${sales}</td>`);
+  }
+  $('tbody tr').append(`<td>${this.totalDailySales}</td>`);
+  $('tbody tr td').attr('class', 'total');
 };
 
 
-
-var newLocationForm = document.getElementById('addLocationForm');
-newLocationForm.addEventListener('submit', handleSubmit);
-
+$('#addLocationForm').on('submit', handleSubmit);
 function handleSubmit(event){
   event.preventDefault();
   var locationcity = event.target.locationcity.value;
   var minCustHr = event.target.minCustHr.value;
   var maxCustHr = event.target.maxCustHr.value;
   var avgCookiesPerCust = event.target.avgCookiesPerCust.value;
+
   var newLocation = new Location(locationcity, minCustHr, maxCustHr, avgCookiesPerCust, [], 0);
   newLocation.avgCookieSales();
   newLocation.totalCookieSales();
   newLocation.renderSalesData();
-  renderTableFoot();
 }
 
-function removeFooter() {
-  var section = document.getElementById('locationSalesLists');
-  var index_to_remove = section.childNodes.length -1;
-  section.removeChild(section.childNodes[index_to_remove]);
-}
-  
 (function displaySales(data) {
   renderTableHead();
+  renderTableFoot();
   for(var i = 0; i < data.length; i++){
-    data[i] = new Location(data[i][0], data[i][1], data[i][2], data[i][3], data[i][4], data[i][5]);
-    data[i].avgCookieSales();
-    data[i].totalCookieSales();
-    data[i].renderSalesData();
-  };
-})(locationData);
+        data[i] = new Location(data[i][0], data[i][1], data[i][2], data[i][3], data[i][4], data[i][5]);
+        data[i].avgCookieSales();
+        data[i].totalCookieSales();
+        data[i].renderSalesData();
+      };
+    })(locationData);
+});
+
+
+
 
